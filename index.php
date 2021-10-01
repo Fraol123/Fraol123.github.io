e4dce0e417d45800e692a8afaee8c887a909df3d
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-10-01 14:34:57 +0300

    lets see

--- index.php
+++ index.php
@@ -19,13 +19,6 @@
 </head>
 <body>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 m-auto">
-                <div class="card mt-5">
-                    <div class="card-title">
-                        <h2 class="text-center py-2"> Contact Us </h2>
-                        <hr>
                         <?php 
                             $Msg = "";
                             if(isset($_GET['error']))
@@ -41,19 +34,5 @@
                             }
  
                         ?>
-                    </div>
-                    <div class="card-body">
-                        <form action="process.php" method="post">
-                            <input type="text" name="UName" placeholder="User Name" class="form-control mb-2">
-                            <input type="email" name="Email" placeholder="Email" class="form-control mb-2">
-                            <input type="text" name="Subject" placeholder="Subject" class="form-control mb-2">
-                            <textarea name="msg" class="form-control mb-2" placeholder="Write The Message"></textarea>
-                            <button class="btn btn-success" name="btn-send"> Send </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </body>
 </html>
