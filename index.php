02824a480c01f756976516a6c05673a3af657e43
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-10-01 14:28:36 +0300

    completed

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta charset="UTF-8">
+ 	<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>FraolTesfaye Portfolio Website</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
@@ -16,10 +13,19 @@
         +Pro:300,900&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="style.css">
-	<link rel="indexpage" href="index.html">
+        <link rel="index.php" href="index.php">
+
+    <title>Contact Us Form In Php</title>
 </head>
 <body>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 m-auto">
+                <div class="card mt-5">
+                    <div class="card-title">
+                        <h2 class="text-center py-2"> Contact Us </h2>
+                        <hr>
                         <?php 
                             $Msg = "";
                             if(isset($_GET['error']))
@@ -33,6 +39,21 @@
                                 $Msg = " Your Message Has Been Sent ";
                                 echo '<div class="alert alert-success">'.$Msg.'</div>';
                             }
+ 
                         ?>
+                    </div>
+                    <div class="card-body">
+                        <form action="process.php" method="post">
+                            <input type="text" name="UName" placeholder="User Name" class="form-control mb-2">
+                            <input type="email" name="Email" placeholder="Email" class="form-control mb-2">
+                            <input type="text" name="Subject" placeholder="Subject" class="form-control mb-2">
+                            <textarea name="msg" class="form-control mb-2" placeholder="Write The Message"></textarea>
+                            <button class="btn btn-success" name="btn-send"> Send </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
