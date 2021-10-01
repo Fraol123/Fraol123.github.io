28e229065fb4767a9e495a906ed527f765c7eef3
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-10-01 14:21:03 +0300

    lets see

--- index.php
+++ index.php
@@ -1,56 +1,38 @@
-<?php 
-
-    if(isset($_POST['btn-send']))
-    {
-       $UserName = $_POST['UName'];
-       $Email = $_POST['Email'];
-       $Subject = $_POST['Subject'];
-       $Msg = $_POST['msg'];
-
-       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
-       {
-           header('location:index.php?error');
-       }
-       else
-       {
-           $to = "fraoltesfaye.ft@gmail.com";
-
-           if(mail($to,$Subject,$Msg,$Email))
-           {
-               header("location:index.php?success");
-           }
-       }
-    }
-    else
-    {
-        header("location:index.php");
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>FraolTesfaye Portfolio Website</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" 
-        integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />  
-	<link rel="index.html" href="index.html">
-        <div class="contact-section">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"
+        integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />
 
-            <h1>Let's connect</h1>
-            <div class="border"></div>
-            <form class="contact-form" action="index.html" method="GET">
-              <input type="text" class="contact-form-text" placeholder="Your name">
-              <input type="email" class="contact-form-text" placeholder="Your email">
-              <input type="text" class="contact-form-text" placeholder="Your phone">
-              <textarea class="contact-form-text" placeholder="Your message"></textarea>
-              <input type="submit" class="contact-form-btn" value="Send">
-            </form>
-          </div>
-	<footer>
-        </footer>
-    </body>
+        <!-- font type can be changed here -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,900|Source+Sans
+        +Pro:300,900&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="style.css">
+	<link rel="indexpage" href="index.html">
+</head>
+<body>
+
+                        <?php 
+                            $Msg = "";
+                            if(isset($_GET['error']))
+                            {
+                                $Msg = " Please Fill in the Blanks ";
+                                echo '<div class="alert alert-danger">'.$Msg.'</div>';
+                            }
+
+                            if(isset($_GET['success']))
+                            {
+                                $Msg = " Your Message Has Been Sent ";
+                                echo '<div class="alert alert-success">'.$Msg.'</div>';
+                            }
+                        ?>
+</body>
 </html>
-
